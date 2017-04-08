a100bef24a395ee1e4904abed07a69a1693bce09
pkovacs1991 <p.kovacs1991@gmail.com>
2017-04-08 18:33:39 +0200

    Static registration done

--- public/js/loginController.js
+++ public/js/loginController.js
@@ -1,6 +1,6 @@
 
 angular.module('onlinePoker').controller('LoginController', ['$scope', '$http', function($scope, $http) {
-    console.log('heloka')
+    console.log('login')
 
 }]);
 
---
+++ public/js/registerController.js
@@ -0,0 +1,6 @@
+
+angular.module('onlinePoker').controller('RegisterController', ['$scope', '$http', function($scope, $http) {
+    console.log('reg')
+
+}]);
+
